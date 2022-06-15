bedde0ff20b4b0537548ba56bc34c8858c324cc3
divluffy <div.luffy2022@gmail.com>
2022-06-16 00:42:05 +0300

    test stripe

--- src/app/pages/patientregister/patientregister.component.ts
+++ src/app/pages/patientregister/patientregister.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { loadStripe, Stripe } from '@stripe/stripe-js';
 
 @Component({
   selector: 'app-patientregister',
   templateUrl: './patientregister.component.html',
-  styleUrls: ['./patientregister.component.css']
+  styleUrls: ['./patientregister.component.css'],
 })
 export class PatientregisterComponent implements OnInit {
+  private stripe: Stripe | null;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    console.log('state stripe ');
+    this.stripe = await loadStripe(
+      'pk_test_51LAfpgJJXXwqLUlGuHy2b3JwNZ556zqI7LILIKbjBUPFDgm6vDNh6l7yEr5wObxdEPggUZEnjkkbIn28Pycn3g4a00suOL159G'
+    );
+
+    const elements = this.stripe?.elements();
+    const card = elements?.create('card');
+
+    card?.mount('#card');
+
+    card?.on('change', (event) => {
+      const displayError = document.getElementById('card-errors');
+      event.error
+        ? (displayError!.textContent = event.error.message)
+        : (displayError!.textContent = '');
+    });
+
+    const button = document.getElementById('button-paid');
+
+    button?.addEventListener('click', async (event) => {
+      const ownerInfo = {
+        owner: {
+          name: 'any name',
+          email: 'div.jo2022@gamil.com',
+          phone: '972597529501',
+        },
+        amount: 35 * 100,
+        currency: 'usd',
+      };
+
+      try {
+        const result = await this.stripe?.createSource(card!, ownerInfo);
+        console.log(result);
+      } catch (err) {
+        console.log('----------------');
+        console.log(err);
+      }
+    });
   }
-
 }
