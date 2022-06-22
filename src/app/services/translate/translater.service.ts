74789edc323e253394f7bcbf423a66e4358b986e
divluffy <div.luffy2022@gmail.com>
2022-06-22 15:08:16 +0300

    gg

--- src/app/services/translate/translater.service.ts
+++ src/app/services/translate/translater.service.ts
@@ -7,12 +7,14 @@ import { TranslateService } from '@ngx-translate/core';
 export class TranslaterService {
   constructor(private translater: TranslateService) {
     let userLang = navigator.language || navigator.userAgent;
-    if (userLang === 'sp') this.translater.use('sp');
+    let fromLangStore = localStorage.getItem('matchetune-language-no-gaza');
+    let checkIs = fromLangStore || userLang;
+    if (checkIs === 'sp') this.translater.use('sp');
     else this.translater.use('en');
-    // add to localstorage
   }
 
   changeLanguage(lang: string) {
     this.translater.use(lang);
+    localStorage.setItem('matchetune-language-no-gaza', lang);
   }
 }
